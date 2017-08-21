198d042345a374634d367f758d49f5a2e98d571e
Yoni Bettan <yonibettan@gmail.com>
2017-08-21 09:29:49 +0300

    changed output vector size

--- 1.2 signed int to offset binary.vhd
+++ 1.2 signed int to offset binary.vhd
@@ -6,7 +6,7 @@ use ieee.std_logic_arith.all
 entity int_to_offset is 
 port (
     din        	:   in std_logic_vector (7 downto 0);
-    dout     	:   out std_logic_vector (3 downto 0);
+    dout     	:   out std_logic_vector (7 downto 0);
 );
 end int_to_offset;
 
